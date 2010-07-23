131eb5c6b168c5a88066a87314ec89a03abdd35e
germain.aubert <germain.aubert@25d42f11-6d70-3e1a-6597-4ab9870d2dd0>
2010-07-23 12:31:45 +0000

    label and icons for the editor and outline

--- org.mgs.graphics.pm.ui/src/org/mgs/graphics/pm/ui/labeling/PmDslDescriptionLabelProvider.java
+++ org.mgs.graphics.pm.ui/src/org/mgs/graphics/pm/ui/labeling/PmDslDescriptionLabelProvider.java
@@ -23,5 +23,5 @@ public class PmDslDescriptionLabelProvider extends DefaultDescriptionLabelProvid
       return ele.getEClass().getName() + ".gif";
     }	 
 */
-
+	
 }
--- org.mgs.graphics.pm.ui/src/org/mgs/graphics/pm/ui/labeling/PmDslLabelProvider.java
+++ org.mgs.graphics.pm.ui/src/org/mgs/graphics/pm/ui/labeling/PmDslLabelProvider.java
@@ -3,8 +3,20 @@
 */
 package org.mgs.graphics.pm.ui.labeling;
 
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
+import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.xtext.ui.label.DefaultEObjectLabelProvider;
+import org.mgs.graphics.pm.pmDsl.BuiltInCall;
+import org.mgs.graphics.pm.pmDsl.ColorDef;
+import org.mgs.graphics.pm.pmDsl.Expression;
+import org.mgs.graphics.pm.pmDsl.INV;
+import org.mgs.graphics.pm.pmDsl.ModuleDef;
+import org.mgs.graphics.pm.pmDsl.OperationDef;
+import org.mgs.graphics.pm.pmDsl.Stage;
+import org.mgs.graphics.pm.pmDsl.Variant;
+import org.mgs.graphics.pm.pmDsl.VariantExpression;
 
 import com.google.inject.Inject;
 
@@ -20,15 +32,42 @@ public class PmDslLabelProvider extends DefaultEObjectLabelProvider {
 		super(delegate);
 	}
 
-/*
-	//Labels and icons can be computed like this:
-	
-	String text(MyModel ele) {
-	  return "my "+ele.getName();
+	String image(Stage e){
+		return "defaulttarget_obj.gif";
 	}
-	 
-    String image(MyModel ele) {
-      return "MyModel.gif";
-    }
-*/
+	
+	String image(ModuleDef e){
+		if(e instanceof OperationDef){
+			return "targetpublic_obj.gif";
+		}
+		return "targetinternal_obj.gif";
+	}
+	
+	String image(EObject e){
+		return "public_co.gif";
+	}
+	
+	private String getClassName(EObject object){
+		return object.eClass().getName();
+	}
+	
+	private StyledString getTextForObject(EObject object, String name){
+		StyledString str = new StyledString();
+		String qualifier = getClassName(object);
+		if(qualifier.endsWith("Def")){
+			qualifier = qualifier.substring(0, qualifier.length() - 3);
+		}
+		str.append(qualifier, StyledString.QUALIFIER_STYLER);
+		str.append(" " + name);
+		return str;
+	}
+	
+	StyledString text(ModuleDef element) {
+		return getTextForObject(element, element.getName());
+	}
+	
+	StyledString text(Stage element) {
+		return getTextForObject(element, element.getName());
+	}
+	
 }
