40ba8647ea34fb0eb548a5090e986aaaf80d12c3
germain.aubert <germain.aubert@25d42f11-6d70-3e1a-6597-4ab9870d2dd0>
2010-07-23 12:31:18 +0000

    avoid operations display in the outline

--- org.mgs.graphics.pm.ui/src/org/mgs/graphics/pm/ui/outline/PmDslTransformer.java
+++ org.mgs.graphics.pm.ui/src/org/mgs/graphics/pm/ui/outline/PmDslTransformer.java
@@ -3,12 +3,28 @@
 */
 package org.mgs.graphics.pm.ui.outline;
 
+import java.util.List;
+
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.ui.editor.outline.transformer.AbstractDeclarativeSemanticModelTransformer;
+import org.mgs.graphics.pm.pmDsl.ModuleDef;
+import org.mgs.graphics.pm.pmDsl.Stage;
 
 /**
  * customization of the default outline structure
  * 
  */
 public class PmDslTransformer extends AbstractDeclarativeSemanticModelTransformer {
+	
+	List<EObject> getChildren(ModuleDef semanticNode) {
+		// Don't display any children.
+		return new BasicEList<EObject>();
+	}
+	
+	List<EObject> getChildren(Stage semanticNode) {
+		// Don't display any children.
+		return new BasicEList<EObject>();
+	}
 	
 }
